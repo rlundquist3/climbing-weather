e82dc073b0c98e5188de6f095cbbad2218ebaa33
Riley Lundquist <rlundquist3@gmail.com>
2025-03-30 11:51:17 -0600

    render in local time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  adapter: vercel(),
 });
